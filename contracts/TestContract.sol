a89d090296dd6b1d4ebd3f7871f54155a8e599ec
Michael Lee <vlmlee2@gmail.com>
2022-09-03 02:03:11 -0500

    Created project

---
+++ contracts/TestContract.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+
+contract TestContract {
+    constructor() {
+        string storage data;
+    }
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+import { ethers } from "hardhat";
+
+async function main() {
+    const Test = await ethers.getContractFactory("TestContract");
+    const test = await Test.deploy();
+
+    await test.deployed();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
